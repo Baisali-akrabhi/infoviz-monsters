793fd4980d1bf0169092490b5748545adb274c6d
baisali pradhan <baisali.pradhan@infoviz.co>
2020-07-28 11:47:33 +0530

    Updates

---
+++ precache-manifest.6e61b5ce7f3feaed4978bfc644c43978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3572d1ca089c568c0ca8f02ad5d210",
+    "url": "/infoviz-monsters/index.html"
+  },
+  {
+    "revision": "70c65f411b4dfc479017",
+    "url": "/infoviz-monsters/static/css/main.0e559982.chunk.css"
+  },
+  {
+    "revision": "40999514a85774c853dc",
+    "url": "/infoviz-monsters/static/js/2.ee00b519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoviz-monsters/static/js/2.ee00b519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c65f411b4dfc479017",
+    "url": "/infoviz-monsters/static/js/main.4e537aa4.chunk.js"
+  },
+  {
+    "revision": "9f59a8dee8781e611c1c",
+    "url": "/infoviz-monsters/static/js/runtime-main.cea38c1a.js"
+  }
+]);
